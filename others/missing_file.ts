b79c62f5a884d3b33dfaedd2a89a2cafff87bd64
Mobin Himu <sdm241405@gamil.com>
2024-01-14 00:21:48 +0600

    others

---
+++ others/missing_file.ts
@@ -0,0 +1,8 @@
+/*
+15 - Type Narrowing
+16 - Working With Type Declarations
+17 - Modules
+19 - React & TypeScript
+
+Those missing file is completed from others course . and  i have had enough knowledge all about!
+*/
